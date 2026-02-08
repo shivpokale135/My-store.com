7fa7f8d8b6890acc8363da65ba0da84622aae9dc
shivpokale135 <shivpokale135@gmail.com>
2026-02-08 22:07:02 +0530

    Add addProduct function to admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,10 @@
 // admin logic will be added later
+function addProduct(name, price, image) {
+  db.collection("products").add({
+    name: name,
+    price: price,
+    image: image
+  }).then(() => {
+    alert("Product added");
+  });
+}
